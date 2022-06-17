6d160cde96b0fec122f58d24ccdc385c80edd2f1
John Gates <jgates@slac.stanford.edu>
2022-06-17 09:43:13 -0700

    Fixed typo.

--- fpgaMain/main.cpp
+++ fpgaMain/main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
     thread thrd([&fpga]() {fpga.run();});
 
     getchar();
-    cout << "stopping loop...\n"
+    cout << "stopping loop...\n";
     fpga.stop();
 
     thrd.join();
